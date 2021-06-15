f6884603a25965a1399044580114004b85c1f183
LucGulden <luc.gulden@viacesi.fr>
2021-06-15 15:06:49 +0200

    Img + txt

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var titre_article = "Article 1"
+//var titre_article = "Article 1"
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        titre_article: 'Article 1',
+        desc_article: 'Voici mon article 1 :)',
+        img: './assets/img/article1.jpg'
+    }
+  })
